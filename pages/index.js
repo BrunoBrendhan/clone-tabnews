c3b6c35834a555521a0755c635c949a17b72ab59
Bruno Brendhan <149737301+BrunoBrendhan@users.noreply.github.com>
2024-12-05 20:30:25 +0000

    pequena alteração

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Osemdedo e o Huguinho são uns lindos &lt;3</h1>
+    return <h1>Osemdedo e o Huguinho são uns lindos ou talvez não &lt;3</h1>
 }
 
 export default Home;
