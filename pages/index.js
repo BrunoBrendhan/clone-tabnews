cbf765110c44d3bd2bed2a1f88f1ce8621516718
Bruno Brendhan <149737301+BrunoBrendhan@users.noreply.github.com>
2025-03-25 23:53:59 +0000

    editorConfig adicionado

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <div><img src="https://pcpjavali.com.br/wp-content/uploads/2021/09/18.png"></img><br></br> <h1>Ainda vou comprar essa belezura</h1></div>
+  return <div><img src="https://pcpjavali.com.br/wp-content/uploads/2021/09/18.png"></img><br></br> <h1>Ainda vou comprar essa belezura</h1></div>
 }
 export default Home;
