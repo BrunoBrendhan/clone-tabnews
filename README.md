0e48ff792748226645e4e6f660ec41f264453c2b
Bruno Brendhan <149737301+BrunoBrendhan@users.noreply.github.com>
2025-03-26 18:04:41 +0000

    adicionado os scripts `lint:check` e `lint:write`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Trilhando os passos para me tornar um programador competente.
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-function Home(){
-  return <div><img src="https://pcpjavali.com.br/wp-content/uploads/2021/09/18.png"></img><br></br> <h1>Ainda vou comprar essa belezura</h1></div>
+function Home() {
+  return (
+    <div>
+      <img src="https://pcpjavali.com.br/wp-content/uploads/2021/09/18.png"></img>
+      <br></br> <h1>Ainda vou comprar essa belezura</h1>
+    </div>
+  );
+}
+function Teste() {
+  console.log("testando Indentação");
 }
 export default Home;
