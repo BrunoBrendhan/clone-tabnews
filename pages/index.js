6fef36cb3e5e5828cd896c64c87c59959481f58c
Bruno Brendhan <149737301+BrunoBrendhan@users.noreply.github.com>
2025-04-04 20:04:29 +0000

    mensagem Hugo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div>
       <img src="https://pcpjavali.com.br/wp-content/uploads/2021/09/18.png"></img>
-      <br></br> <h1>Ainda vou comprar essa belezura</h1>
+      <br></br> <h1>Olá Huginho Bertochinho</h1>
     </div>
   );
 }
