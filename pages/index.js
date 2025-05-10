0df9a1952be97c2754e5fe72d5c4127d2717cb7a
Bruno Brendhan <149737301+BrunoBrendhan@users.noreply.github.com>
2025-05-10 23:06:21 +0000

    work in progress

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div>
       <img src="https://pcpjavali.com.br/wp-content/uploads/2021/09/18.png"></img>
-      <br></br> <h1>Olá Huginho Bertochinho</h1>
+      <br></br> <h1>Em construção</h1>
     </div>
   );
 }
